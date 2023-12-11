0c2bae852241610d8839f7db4343833ee17187f9
admin <admin@DESKTOP-CNLFK9P>
2023-12-11 13:59:30 +0300

    Добавьте файлы проекта.

---
+++ gittest/gittest.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    double num;
+    std::cout << "Введите число: ";
+    std::cin >> num;
+
+    double result = num * 20;
+
+    std::cout << "Результат: " << result << std::endl;
+
+    return 0;
+}
